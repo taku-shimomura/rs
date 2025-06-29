c657299d9081d1c66a5ec2e548287e10a218f20e
Taku <23535528+taku-shimomura@users.noreply.github.com>
2025-06-29 18:57:51 +0900

    q

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,54 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::cell::RefCell;
+use std::rc::Rc;
+
+type Link<T> = Option<Rc<RefCell<Node<T>>>>;
+
+#[derive(Debug)]
+pub struct Node<T> {
+    val: T,
+    next: Link<T>,
+    prev: Link<T>,
+}
+
+impl<T> Node<T> {
+    pub fn new(val: T) -> Self {
+        Self {
+            val,
+            next: None,
+            prev: None,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Queue<T> {
+    head: Link<T>,
+    tail: Link<T>,
+}
+
+impl<T> Queue<T> {
+    pub fn new() -> Self {
+        Self {
+            head: None,
+            tail: None,
+        }
+    }
+
+    pub fn enqueue(&mut self, val: T) {
+        todo!("未実装");
+    }
+
+    pub fn dequeue(&mut self) {
+        todo!("未実装");
+    }
+
+    pub fn front(&mut self) -> Option<T> {
+        todo!("未実装");
+    }
+
+    pub fn back(&mut self) -> Option<T> {
+        todo!("未実装");
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +56,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn queue() {
+        let mut q = Queue::new();
+        for i in 1..=5 {
+            q.enqueue(i);
+        }
+        assert_eq!(q.front(), Some(1));
+        assert_eq!(q.back(), Some(5));
+        for _ in 0..3 {
+            q.dequeue();
+        }
+        assert_eq!(q.front(), Some(4));
+        assert_eq!(q.back(), Some(5));
+        for _ in 0..2 {
+            q.dequeue();
+        }
+        assert_eq!(q.front(), None);
+        assert_eq!(q.back(), None);
     }
 }
