7386a945e14f2d022042fee38f7cc248c1cb9621
Taku <23535528+taku-shimomura@users.noreply.github.com>
2025-06-29 22:13:54 +0900

    refcell

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,19 @@
 pub mod ds;
+
+#[cfg(test)]
+mod tests {
+    use std::rc::Rc;
+    use std::cell::RefCell;
+
+    #[test]
+    fn rc() {
+        #[derive(Default)]
+        struct User {
+            username: String,
+        }
+        let u = Rc::new(RefCell::new(User { username: String::from("user1") }));
+        assert_eq!(u.borrow().username, String::from("user1"));
+        u.borrow_mut().username = String::from("updated user name");
+        assert_eq!(u.borrow().username, String::from("updated user name"));
+    }
+}
